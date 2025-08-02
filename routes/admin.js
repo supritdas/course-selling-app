2ce208ce3b54702526b5c4d280dad0dc7832f347
supritdas <supritdas789@gmail.com>
2025-08-02 12:17:20 +0530

    added admin router

---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+
+adminRouter.post("/signup", function (req, res){
+    res.json({
+        message: "signup endpoint"
+    })
+})
+
+adminRouter.post("/signin", function (req, res){
+    res.json({
+        message: "signin endpoint"
+    })
+})
+
+adminRouter.use(adminMiddleware);
+
+adminRouter.post("/course", function (req, res){
+    res.json({
+        message: "create course"
+    })
+})
+
+adminRouter.put("/course", function (req, res){
+    res.json({
+        message: "create course"
+    })
+})
+
+adminRouter.get("/course/bulk", function (req, res){
+    res.json({
+        message: "create course"
+    })
+})
+
+module.exports = {
+    adminRouter : adminRouter
+}
